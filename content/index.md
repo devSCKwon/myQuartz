ab4a95c8c319456f4da466b86297723dbad8ef93
devSCKwon <shry19@gmail.com>
2025-06-16 14:45:05 +0900

    myQuartz 이동

--- content/index.md
+++ content/index.md
@@ -2,7 +2,7 @@
 title: Welcome to Quartz
 ---
 
-테스트
+Welcome SCKwon's Quartz.
 
 This is a blank Quartz installation.
 See the [documentation](https://quartz.jzhao.xyz) for how to get started.
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "🪴SEJIN Manual",
+    pageTitleSuffix: "세진문서모음",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
