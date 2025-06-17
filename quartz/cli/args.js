0fa9c13aac2a92dedc6484d55aa0572a82ddf286
devSCKwon <shry19@gmail.com>
2025-06-17 10:12:23 +0900

    contents 폴더 위치 변경
    - Obsidian 에서 취급하기 위한 폴더로 명칠 변경

--- quartz/cli/args.js
+++ quartz/cli/args.js
@@ -2,7 +2,7 @@ export const CommonArgv = {
   directory: {
     string: true,
     alias: ["d"],
-    default: "content",
+    default: "myQuartz_content",
     describe: "directory to look for content files",
   },
   verbose: {
